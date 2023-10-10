dd1be123d05cc3c3455755e4fa0118e4e504b306
JOO <yousefkarem91@gmail.com>
2023-10-10 03:48:27 +0300

    Add ID numbers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+// FCAI – OOP Programming – 2023 - Assignment 1
+// Program Name:				Filters.cpp
+// Last Modification Date:	10/10/2023
+// Author1 and ID and Group:	20220402
+// Author2 and ID and Group:	20220052
+// Author3 and ID and Group:	20220396
+// Purpose: Program Filers to apply some edits for images
+
 #include<bits/stdc++.h>
 #include "Filters.h"
 using namespace std;
